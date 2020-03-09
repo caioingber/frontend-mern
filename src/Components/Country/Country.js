24417608552854683612dad0a844a93f0c8423b1
caio_ingber <caioingber@gmail.com>
2020-03-08 22:21:20 -0400

    styled country

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -32,23 +32,51 @@ function Country(props) {
 
   return (
     <div className="flex column">
-      <h2 className="margin">{locale[0].country.name}</h2>
+      <h2 className="margin title">{locale[0].country.name}</h2>
       <div className="country-pic bottom-margin cover" style={backgroundImg}>
         {" "}
         {/* <img src={locale[0].country.flag} className="fill"></img> */}
       </div>
-      <div>Population: {locale[0].country.population}</div>
-      <div style={showColor(gdp, 17300)}>GDP Per Capita(USD): ${gdp}</div>
-      <div style={showColor(locale[0].hdi, 0.7)}>HDI: {locale[0].hdi}</div>
-      <div style={showColor(locale[0].country.gini, 50)}>
-        Gini Coefficient: {locale[0].country.gini}
+      <div className="bottom-margin flex report">
+        <div className="flex column">
+          <div className="bottom-margin stat">
+            Population: {locale[0].country.population}
+          </div>
+          <div style={showColor(gdp, 17300)} className="bottom-margin stat">
+            GDP Per Capita(USD): ${gdp}
+          </div>
+          <div
+            style={showColor(locale[0].hdi, 0.7)}
+            className="bottom-margin stat"
+          >
+            HDI: {locale[0].hdi}
+          </div>
+        </div>
+        <div className="flex column">
+          {" "}
+          <div
+            style={showColor(locale[0].country.gini, 50)}
+            className="bottom-margin stat"
+          >
+            Gini Coefficient: {locale[0].country.gini}
+          </div>
+          <div className="bottom-margin stat">
+            Biocapacity: {locale[0].totalBiocapacity}
+          </div>
+          <div
+            style={showColor(locale[0].biocapacityReserve, 0)}
+            className="bottom-margin stat"
+          >
+            {" "}
+            Biocapacity Deficit/Reserve: {locale[0].biocapacityReserve}
+          </div>
+        </div>
       </div>
-      <div>Biocapacity: {locale[0].totalBiocapacity}</div>
-      <div style={showColor(locale[0].biocapacityReserve, 0)}>
-        {" "}
-        Biocapacity Deficit/Reserve: {locale[0].biocapacityReserve}
-      </div>
-      <Link to={`/region/${locale[0].country.region}`} className="back-btn">
+
+      <Link
+        to={`/region/${locale[0].country.region}`}
+        className="btn btn-outline-primary back"
+      >
         Back to Region
       </Link>
     </div>
